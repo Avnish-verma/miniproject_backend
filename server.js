38be6504d8d4aaf1c4e0b4b2da34e9f4c88204cd
AVNISH KUMAR VERMA <a33010171@gmail.com>
2026-08-08 11:47:32 +0530

    adding cors

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express= require('express');
 const app = express();
+const cors = require("cors");
+
+app.use(cors({
+    origin: "*"
+}));
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 app.use(express.json());
